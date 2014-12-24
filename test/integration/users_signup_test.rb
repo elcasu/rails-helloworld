323edf79ea47567f528ff3cdda07c54813b4e304
Casu <info@casu-net.com.ar>
2014-12-24 09:25:29 -0300

    Finish user signup

---
+++ test/integration/users_signup_test.rb
@@ -0,0 +1,29 @@
+require 'test_helper'
+
+class UsersSignupTest < ActionDispatch::IntegrationTest
+    test "invalid signup information" do
+        get signup_path
+        assert_no_difference 'User.count' do
+            post users_path, user: {
+                name: "",
+                email: "user@invalid",
+                password: "123",
+                password_confirmation: "321"
+            }
+        end
+        assert_template 'users/new'
+    end
+
+    test "valid signup information" do
+        get signup_path
+        assert_difference 'User.count', 1 do
+            post_via_redirect users_path, user: {
+                name: "Example user",
+                email: "newuser@example.com",
+                password: "123123123",
+                password_confirmation: "123123123"
+            }
+        end
+        assert_template 'users/show'
+    end
+end
