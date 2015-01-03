49c290bf150d82a1f33b6206a3276d4381881518
Casu <info@casu-net.com.ar>
2015-01-03 03:51:41 -0300

    Finish login/logout

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ActiveRecord::Base
+    attr_accessor :remember_token
     # Validations
     VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
 
@@ -17,4 +18,32 @@ class User < ActiveRecord::Base
 
     # Authentication built-in features
     has_secure_password
+
+    # Returns the hash digest of the given string
+    def User.digest(string)
+        cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST : BCrypt::Engine.cost
+        BCrypt::Password.create(string, cost: cost)
+    end
+
+    # Generate a random token
+    def User.new_token
+        SecureRandom.urlsafe_base64
+    end
+
+    # Remembers users in the database
+    def remember
+        self.remember_token = User.new_token
+        update_attribute(:remember_digest, User.digest(remember_token))
+    end
+
+    # Returns true if the given token matches the digest.
+    def authenticated?(remember_token)
+        return false if remember_digest.nil?
+        BCrypt::Password.new(remember_digest).is_password?(remember_token)
+    end
+
+    # Forgets a user
+    def forget
+        update_attribute(:remember_digest, nil)
+    end
 end
